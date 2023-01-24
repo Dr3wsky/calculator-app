f52c1e4705e6b289ba17915725e6d3121eb6e1a1
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-24 10:35:03 -0700

    created core operator functions

---
+++ calculator-funcs.js
@@ -0,0 +1,32 @@
+/*  Set core operator functions */
+
+function add(num1, num2) {
+    return (num1 + num2);
+};
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+};
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+};
+
+function divide(num1, num2) {
+    return (num1 / num2);
+};
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return null;
+    }
+};
