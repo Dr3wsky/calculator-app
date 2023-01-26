c78e28f82f525700d56024b5586ec3cdfcb2c4fa
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-26 14:15:54 -0700

    updating operation functions

--- calculator-funcs.js
+++ calculator-funcs.js
@@ -1,22 +1,46 @@
-/* Initialize DOM elements */
+/* Initialize DOM elements and vars */
 let history = document.getElementById("history");
 let input = document.getElementById("input");
+let operator = '';
+const operatorArray = ['/', '*', '+', '-'];
 const nums = Array.from(document.querySelectorAll('.btn-num'));
 const funcs = Array.from(document.querySelectorAll('.btn-func'));
 
 /* Create event listeners */
-nums.forEach(n => n.addEventListener('click', updateInput));
-funcs.forEach(f => f.addEventListener('click', updateOperator));
+nums.forEach(n => n.addEventListener('click', numInput));
+funcs.forEach(f => f.addEventListener('click', operatorInput));
 
 /* Update screen from event listener inputs */
-function updateInput(e) {
-    if (input.textContent.length < 10) {
-        input.textContent += e.target.textContent;
+function numInput(e) {
+    if (input.textContent.length == 1 && input.textContent == '0') {
+        return input.textContent = e.target.textContent;
     }
-    // add class to flash screen background red for too long of an entry
+    if (input.textContent.length == 1 && input.textContent !== '0') {
+        return input.textContent += e.target.textContent;
+    }
+    if (input.textContent.length > 1 && input.textContent.length < 10) {
+        return input.textContent += e.target.textContent;
+    }
+};
+    // add class to flash screen background red for too long of an entr};
+
+function operatorInput(e) {
+    if (operatorArray.includes(e.target.id) && operator === '') {
+        operator = e.target.id;
+        input.textContent = operator + input.textContent;
+    }
+    else if (operatorArray.includes(e.target.id) && operator !== '') {
+        history.textContent = operate(operator, Number(history.textContent), Number(input.textContent.slice(1)));
+        clearInput();
+        operator = e.target.id;
+        input.textContent = operator + input.textContent;
+    }
+    else {
+        nonMathFunc();
+    }
 };
 
-function updateOperator(e) {
+function nonMathFunc(e) {
     switch (e.target.id) {
         case 'clear-all':
             clearAll();
@@ -66,13 +90,18 @@ function operate(operator, num1, num2) {
 
 function deleteChar() {
     input.textContent = input.textContent.slice(0, -1);
+    if (input.textContent = '') {
+        input.textContent = '0';
+    }
 };
 
 function clearInput() {
-    input.textContent = '';
+    input.textContent = '0';
+    operator = '';
 };
 
 function clearAll() {
-    input.textContent = '';
+    input.textContent = '0';
     history.textContent = '';
+    operator = '';
 };
