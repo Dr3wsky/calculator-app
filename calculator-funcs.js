eada436f1278415f6f1e031bb64f2e6a87d7940d
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-27 14:04:38 -0700

    commented fucntions logic

--- calculator-funcs.js
+++ calculator-funcs.js
@@ -1,4 +1,4 @@
-/* Initialize DOM elements and vars */
+/* Initialize DOM elements and global vars */
 let history = document.getElementById("history");
 let input = document.getElementById("input");
 let operator = '';
@@ -11,20 +11,26 @@ const funcs = Array.from(document.querySelectorAll('.btn-func'));
 nums.forEach(n => n.addEventListener('click', numInput));
 funcs.forEach(f => f.addEventListener('click', operatorInput));
 
-/* Update screen from event listener inputs */
+/* Complex logic functions for display and event listener actions */
+
+// Logic when number is pressed
 function numInput(e) {
+    // If no input yet, erase placeholder zero and add number input
     if (inputActive == false) {
         input.textContent = input.textContent.slice(0, 0);
         input.textContent += e.target.textContent;
         inputActive = true;
     }
+    // Continue adding numbers to existing inpout until screen midth reached
     else if (inputActive == true && input.textContent.length <12) {
         input.textContent += e.target.textContent;
     }
 };
     // add class to flash screen background red for too long of an entr};
 
+//Logic when non-number button is pressed
 function operatorInput(e) {
+    // If no operator is logged, set operator and display on screen at front of input
     if (operatorArray.includes(e.target.id) && operator === '') {
         operator = e.target.id;
         if (input.textContent == '0') {
@@ -33,18 +39,21 @@ function operatorInput(e) {
         input.textContent = operator + input.textContent;
         inputActive = true;
     }
-    else if (operatorArray.includes(e.target.id) && operator !== '') {
+    // If an operator is already logged, run the active computation, then log and display new operator
+    else if (operatorArray.includes(e.target.id) && operator !== '') {  
         history.textContent = operate(operator, Number(history.textContent), Number(input.textContent.slice(1)));
         clearInput();
         operator = e.target.id;
         input.textContent = operator;
         inputActive = true;
     }
+    // if no operator is pressed, jump to display modification functions
     else {
         nonMathFunc(e);
     }
 };
 
+// switch function to check the non-operator function to run computation and/or update screen
 function nonMathFunc(e) {
     switch (e.target.id) {
         case 'clear-all':
@@ -60,8 +69,23 @@ function nonMathFunc(e) {
     }
 };
 
+//Switch to core operator function based on logged operator symbol
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return add(num1, num2);
+        case '×':
+            return multiply(num1, num2);
+        case '÷':
+            return divide(num1, num2);
+        default:
+            return null;
+    }
+};
 
-/*  Set core operator functions */
+/*  Core math operator functions */
 function add(num1, num2) {
     return (num1 + num2);
 };
@@ -78,21 +102,8 @@ function divide(num1, num2) {
     return (num1 / num2);
 };
 
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return add(num1, num2);
-        case '×':
-            return multiply(num1, num2);
-        case '÷':
-            return divide(num1, num2);
-        default:
-            return null;
-    }
-};
 
+/*  Display modification functions */
 function deleteChar() {
     input.textContent = input.textContent.slice(0, -1);
     if (input.textContent == '') {
